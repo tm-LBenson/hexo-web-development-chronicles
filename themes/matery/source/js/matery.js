225b518317b3f90410fcecdef59391cfca4dd2c8
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-03 10:05:14 -0500

    edit changes to dark mode to fix style issues

--- themes/matery/source/js/matery.js
+++ themes/matery/source/js/matery.js
@@ -1,7 +1,5 @@
 $(function () {
-    /**
-     * 添加文章卡片hover效果.
-     */
+    
     let articleCardHover = function () {
         let animateClass = 'animated pulse';
         $('article .article').hover(function () {
@@ -12,10 +10,10 @@ $(function () {
     };
     articleCardHover();
 
-    /*菜单切换*/
+    
     $('.sidenav').sidenav();
 
-    /* 修复文章卡片 div 的宽度. */
+  
     let fixPostCardWidth = function (srcId, targetId) {
         let srcDiv = $('#' + srcId);
         if (srcDiv.length === 0) {
@@ -35,16 +33,12 @@ $(function () {
         $('#' + targetId).width(w);
     };
 
-    /**
-     * 修复footer部分的位置，使得在内容比较少时，footer也会在底部.
-     */
+ 
     let fixFooterPosition = function () {
         $('.content').css('min-height', window.innerHeight - 165);
     };
 
-    /**
-     * 修复样式.
-     */
+  
     let fixStyles = function () {
         fixPostCardWidth('navContainer');
         fixPostCardWidth('artDetail', 'prenext-posts');
@@ -52,12 +46,11 @@ $(function () {
     };
     fixStyles();
 
-    /*调整屏幕宽度时重新设置文章列的宽度，修复小间距问题*/
     $(window).resize(function () {
         fixStyles();
     });
 
-    /*初始化瀑布流布局*/
+  
     $('#articles').masonry({
         itemSelector: '.article'
     });
@@ -68,20 +61,20 @@ $(function () {
         delay: 100
     });
 
-    /*文章内容详情的一些初始化特性*/
+  
     let articleInit = function () {
         $('#articleContent a').attr('target', '_blank');
 
         $('#articleContent img').each(function () {
             let imgPath = $(this).attr('src');
             $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
-            // 图片添加阴影
+          
             $(this).addClass("img-shadow img-margin");
-            // 图片添加字幕
+         
             let alt = $(this).attr('alt');
             let title = $(this).attr('title');
             let captionText = "";
-            // 如果alt为空，title来替
+         
             if (alt === undefined || alt === "") {
                 if (title !== undefined && title !== "") {
                     captionText = title;
@@ -89,7 +82,7 @@ $(function () {
             } else {
                 captionText = alt;
             }
-            // 字幕不空，添加之
+          
             if (captionText !== "") {
                 let captionDiv = document.createElement('div');
                 captionDiv.className = 'caption';
@@ -118,19 +111,19 @@ $(function () {
 
     $('.modal').modal();
 
-    /*回到顶部*/
+  
     $('#backTop').click(function () {
         $('body,html').animate({scrollTop: 0}, 400);
         return false;
     });
 
-    /*监听滚动条位置*/
+    
     let $nav = $('#headNav');
     let $backTop = $('.top-scroll');
-    // 当页面处于文章中部的时候刷新页面，因为此时无滚动，所以需要判断位置,给导航加上绿色。
+  
     showOrHideNavBg($(window).scrollTop());
     $(window).scroll(function () {
-        /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
+
         let scroll = $(window).scrollTop();
         showOrHideNavBg(scroll);
     });
